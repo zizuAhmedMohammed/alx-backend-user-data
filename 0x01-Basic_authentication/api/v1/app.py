63125106821116ffcd7bb4395a9fa169aabe2b91
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-05-03 19:57:00 +0000

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,39 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth_type = getenv('AUTH_TYPE', 'auth')
+auth = None
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.before_request
+def before_request() -> str:
+    """Before request huandler
+    """
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if auth.require_auth(request.path, excluded_paths):
+        auth_header = auth.authorization_header(request)
+        user = auth.current_user(request)
+        if auth_header is None:
+            abort(401)
+        if user is None:
+            abort(403)
 
 
 @app.errorhandler(404)
