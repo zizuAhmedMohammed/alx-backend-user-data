38d1e1c08aef054dc9d0bb49d48ec740dc4ce1fa
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-05-04 22:21:59 +0000

    Create Session Auth and Modify App.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,18 +7,22 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth_type = getenv('AUTH_TYPE', 'auth')
 auth = None
+if auth_type == 'auth':
+    auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-else:
-    auth = Auth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.before_request
